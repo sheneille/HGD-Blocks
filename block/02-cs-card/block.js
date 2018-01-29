1a6122bbcb848df6314c35e803f1556a29d43f25
sheneillepatil <sheneillepatil@gmail.com>
2018-01-29 22:32:42 +0530

    Modify index.php and block.js

--- block/02-cs-card/block.js
+++ block/02-cs-card/block.js
@@ -1,8 +1,30 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates.
- * and open the template in the editor.
+/**
+ * BLOCK: cscard
+ * Registering a editable block with Gutenberg
+ * Styles:editor.css — Editor styles for the block.
+ *        style.css — Frontend styles for the block.
  */
 
-
-
+function(){
+    var __ = wp.i18n.__;
+    var el = wp.element;
+    var blocks = wp.blocks.Editable;
+    var children = wp.blocks.source.children;
+    var registerBlockType = wp.blocks.registerBlockType;
+    var attr = wp.blocks.source.attr;
+    var mediaUploadButton = wp.blocks.MediaUploadButton;
+    
+    /**
+     * Register Editablelist Block.
+     *
+     * Registers a new block provided a unique name and an object defining its
+     * behavior. Once registered, the block is made available as an option to any
+     * editor interface where blocks are implemented.
+     *
+     * @param  {string}   name     Block name.
+     * @param  {Object}   settings Block settings.
+     * @return {?WPBlock}          The block, if it has been successfully
+     *                             registered; otherwise `undefined`.
+     */
+    
+};
