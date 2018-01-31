3f759a29ec63951992ec2456d534175d9f36def0
Shantanu <shantanu2846@gmail.com>
2018-01-31 15:52:20 +0530

    Add edit function in block.js for Block02

--- block/03-button-text/block.js
+++ block/03-button-text/block.js
@@ -13,8 +13,25 @@
 		children: 'p'
 	    }
 	},
-	
+
+	edit: function ( props ) {
+	    var content = props.attributes.content;
+	    var focus = props.focus;
+	    var onChangeContent = function ( newContent ) {
+		props.setAttributes( { content: newContent } );
+	    };
+	    
+	    return element.createElement( blocks.Editable, {
+		tagName: 'p',
+		className: props.className,
+		onChange: onChangeContent,
+		value: content,
+		focus: focus,
+		onFocus: props.SetFocus
+		},
+	    );
+	}
     }
-	)
+	);
     
 } )();
